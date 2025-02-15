be816c7c4bfd42318b3829616617ffc32c0a9989
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-15 10:52:21 +0600

    Max num between 2 elems

---
+++ max.js
@@ -0,0 +1,9 @@
+// Find the max number between 2
+let person1 = 30;
+let person2 = 40;
+if(person1 > person2){
+    console.log("This person is the greatest one", person1)
+}
+else{
+    console.log("This person is the greatest", person2)
+}
