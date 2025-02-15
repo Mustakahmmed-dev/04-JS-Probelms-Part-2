394a69856823d18cb78fbdd9f94632cdbffdd750
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-15 11:00:19 +0600

    Max num between 3 elems using condition

---
+++ max3.js
@@ -0,0 +1,13 @@
+// Find the greatest num from the 3 elements
+let num1 = 30;
+let num2 = 90;
+let num3 = 80;
+if(num1 > num2 && num1 > num3){
+    console.log("Num1 is the greatest ", num1)
+}
+else if(num2 > num1 && num2 > num3){
+    console.log("Num2 is the greatest ", num2)
+}
+else{
+    console.log("Num3 is the greatest ", num3)
+}
