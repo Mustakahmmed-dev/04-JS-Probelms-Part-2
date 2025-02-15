8954294d0ba62e5834f6ce4b1182c5770eab25d9
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-15 11:05:10 +0600

    max num using fun and cond for 3 els

--- max3.js
+++ max3.js
@@ -11,3 +11,18 @@ else if(num2 > num1 && num2 > num3){
 else{
     console.log("Num3 is the greatest ", num3)
 }
+
+// Using function
+function getMaxNum(num1, num2, num3){
+    if(num1 > num2 && num1 > num3){
+        return num1;
+    }
+    else if(num2 > num1 && num2 > num3){
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+let maxNum = getMaxNum(40, 50, 140);
+console.log("greatest number is ", maxNum);
